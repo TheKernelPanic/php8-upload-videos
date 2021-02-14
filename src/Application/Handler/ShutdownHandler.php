c4176217bbf7fb4944f7bdc0bbe04402f1d69621
Kernel_Panic <jgg.jobs.development@gmail.com>
2021-02-14 22:17:59 +0000

    Add shutdown handler for slim

---
+++ src/Application/Handler/ShutdownHandler.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Application\Handler;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Exception\HttpInternalServerErrorException;
+use Slim\ResponseEmitter;
+
+/**
+ * Class ShutdownHandler
+ * @package Src\Application\Handler
+ */
+class ShutdownHandler
+{
+    /**
+     * ShutdownHandler constructor.
+     * @param ServerRequestInterface $request
+     * @param HttpErrorHandler $httpErrorHandler
+     * @param bool $displayErrorDetails
+     * @param bool $logErrors
+     * @param bool $logErrorDetails
+     */
+    public function __construct(
+        private ServerRequestInterface $request,
+        private HttpErrorHandler $httpErrorHandler,
+        private bool $displayErrorDetails,
+        private bool $logErrors = false,
+        private bool $logErrorDetails = false
+    ){}
+
+    /**
+     *
+     */
+    public function __invoke(): void
+    {
+        $error = error_get_last();
+
+        if (!$error) {
+            return;
+        }
+
+        $exception = new HttpInternalServerErrorException(
+            request: $this->request,
+            message: $error['message']
+        );
+        $response = $this->httpErrorHandler->__invoke(
+            request: $this->request,
+            exception: $exception,
+            displayErrorDetails: $this->displayErrorDetails,
+            logErrors: $this->logErrors,
+            logErrorDetails: $this->logErrorDetails
+        );
+
+        if (ob_get_length()) {
+            ob_clean();
+        }
+
+        $responseEmitter = new ResponseEmitter();
+        $responseEmitter->emit(response: $response);
+    }
+}
